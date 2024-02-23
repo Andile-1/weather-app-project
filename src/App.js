52b38634332c4a90d12dd051505636e3e5efe036
Andile-1 <159252369+Andile-1@users.noreply.github.com>
2024-02-23 13:49:56 +0200

    added personal links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.Andile.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Andile Jili
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Andile-1/weather-project"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://suspicious-beaver-111c4d.netlify.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
