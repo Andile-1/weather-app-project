4b8b8bad72a5180aaea6b4aba3cfd029f918346d
Andile-1 <159252369+Andile-1@users.noreply.github.com>
2024-02-23 13:58:48 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
         <footer>
           This project was coded by{" "}
           <a
